0065c705f2463529e1e54a20f30e384b93df2024
sachin kannaujiya <82204125+sachinkannaujiya98@users.noreply.github.com>
2022-07-30 17:41:41 +0530

    Update Info.js

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -6,13 +6,8 @@ const Info = () => {
       <div className='mb-20'>
         <div>
           <h4 className=' text-[#52DEE5] font-Sora text-3xl mt-32'>Hi, my name is</h4>
-<<<<<<< HEAD
-          <h1 className='text-7xl font-bold font-Sora my-3 text-[#C7D2FC]'>Avnish Tomar</h1>
-          <h2 className='text-[#A1ABBF] font-Sora text-6xl font-medium my-2 leading-0'>I build amazing websites!</h2>
-=======
           <h1 className='text-4xl sm:text-7xl font-bold font-Sora my-3 text-[#C7D2FC]'>Sachin Kannaujiya</h1>
           <h2 className='text-[#A1ABBF] font-Sora text-3xl sm:text-6xl font-medium my-2 leading-0'>I build amazing websites!</h2>
->>>>>>> 4d1d20ad44cc3b3b02f6b203af492d765beea1fa
         </div>
         <div className='sm:w-7/12'>
           <p className='text-[#A1ABBF] text-base leading-6 font-medium font-Sora my-4'>I’m a <strong className='text-[#52DEE5]'>FullStack web developer.</strong> Focused on building products that will make people's lives better. I also create content in form of blogs and threads. I love to learn new things and I am always open to learn new technologies.</p>
